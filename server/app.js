66c8fbe1301ffc48e156f131c30620a4048fda79
Sanjeev Ghimire <sanjeev.ghimire@ibm.com>
2018-09-14 14:58:00 -0500

    linting

--- server/app.js
+++ server/app.js
@@ -10,110 +10,109 @@ const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-l
 require('dotenv').config();
 
 let languageTranslator = new LanguageTranslatorV3({
-    version: "2018-05-01"
+  version: "2018-05-01"
 });
 
 let natural_language_understanding = new NaturalLanguageUnderstandingV1({
-    version: '2018-03-16'
+  version: '2018-03-16'
 });
-    
+
 if (fs.existsSync("/opt/lt-service-bind/binding")) {
-    const binding = JSON.parse(fs.readFileSync("/opt/lt-service-bind/binding", "utf8"));
-    languageTranslator = new LanguageTranslatorV3({
-        iam_apikey: binding.apikey,
-        url: binding.url,
-        version: "2018-05-01"
-    });
-};
+  const binding = JSON.parse(fs.readFileSync("/opt/lt-service-bind/binding", "utf8"));
+  languageTranslator = new LanguageTranslatorV3({
+    iam_apikey: binding.apikey,
+    url: binding.url,
+    version: "2018-05-01"
+  });
+}
 
 if (fs.existsSync('/opt/nlu-service-bind/binding')) {
-    const nlubinding = JSON.parse(fs.readFileSync('/opt/nlu-service-bind/binding', 'utf8'));
+  const nlubinding = JSON.parse(fs.readFileSync('/opt/nlu-service-bind/binding', 'utf8'));
 
-    if (nlubinding.username) {
-        natural_language_understanding = new NaturalLanguageUnderstandingV1({
-        username: nlubinding.username,
-        password: nlubinding.password,
-        url: nlubinding.url,
-        version: '2018-03-16'
-      });
-    }
-    else {
-        natural_language_understanding = new NaturalLanguageUnderstandingV1({
-        iam_apikey: nlubinding.apikey,
-        url: nlubinding.url,
-        version: '2018-03-16'
-      });
-    }
-};
+  if (nlubinding.username) {
+    natural_language_understanding = new NaturalLanguageUnderstandingV1({
+      username: nlubinding.username,
+      password: nlubinding.password,
+      url: nlubinding.url,
+      version: '2018-03-16'
+    });
+  } else {
+    natural_language_understanding = new NaturalLanguageUnderstandingV1({
+      iam_apikey: nlubinding.apikey,
+      url: nlubinding.url,
+      version: '2018-03-16'
+    });
+  }
+}
 
-application.get("/", function (req, response) {    
-    response.json({
-        'message': 'Welcome to Snap and Translate app.'
-    })
+application.get("/", function (req, response) {
+  response.json({
+    'message': 'Welcome to Snap and Translate app.'
+  })
 });
 
 application.post("/uploadpic", function (req, result) {
-    const form = new formidable.IncomingForm();
-    form.keepExtensions = true;
-    form.parse(req, function (err, fields, files) {
+  const form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, function (err, fields, files) {
+    if (err) {
+      console.log(err);
+    } else {
+      const fieldValue = JSON.parse(JSON.stringify(fields));
+      const options = {
+        l: fieldValue.source,
+        psm: 1
+      };
+      const filePath = JSON.parse(JSON.stringify(files));
+      const imgPath = filePath.myPhoto.path;
+      tesseract.process(imgPath, options, function (err, ocrtext) {
         if (err) {
-            console.log(err);
+          console.log(err);
         } else {
-            const fieldValue = JSON.parse(JSON.stringify(fields));
-            const options = {
-                l: fieldValue.source,
-                psm: 1
-            };
-            const filePath = JSON.parse(JSON.stringify(files));
-            const imgPath = filePath.myPhoto.path;
-            tesseract.process(imgPath, options, function (err, ocrtext) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    const parameters = {
-                        text: ocrtext.toLowerCase(),
-                        model_id: fieldValue.modelid
-                    };
-                    languageTranslator.translate(parameters, function (error, response) {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            console.log(JSON.stringify(response, null, 4));
-                            const labelsvr = response.translations[0].translation;
-                            console.log(labelsvr);
-                            let cleanString = labelsvr.replace(/\n/g, '');                            
-                            console.log(cleanString); 
-                            const params = {
-                                'text': labelsvr,
-                                'features': {
-                                    sentiment: {},
-                                    emotion: {}
-                                    }
-                                };
-                            natural_language_understanding.analyze(params, function(err, nluresponse) {
-                            if (err){
-                            console.log('error:', err);
-                            }else{
-                            const sentresp = JSON.stringify(nluresponse.sentiment.document.label);
-                            console.log(sentresp);
-                            const emotoutput = JSON.stringify(nluresponse.emotion.document.emotion);
-                            console.log(emotoutput);
-                            result.json({
-                                data: cleanString,
-                                ocropt: ocrtext,
-                                sentiment: sentresp,
-                                emotion: emotoutput
-                                    });      
-                                }
-                            }); 
-                        }
-                    });
+          const parameters = {
+            text: ocrtext.toLowerCase(),
+            model_id: fieldValue.modelid
+          };
+          languageTranslator.translate(parameters, function (error, response) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log(JSON.stringify(response, null, 4));
+              const labelsvr = response.translations[0].translation;
+              console.log(labelsvr);
+              let cleanString = labelsvr.replace(/\n/g, '');
+              console.log(cleanString);
+              const params = {
+                'text': labelsvr,
+                'features': {
+                  sentiment: {},
+                  emotion: {}
                 }
-            });
+              };
+              natural_language_understanding.analyze(params, function (err, nluresponse) {
+                if (err) {
+                  console.log('error:', err);
+                } else {
+                  const sentresp = JSON.stringify(nluresponse.sentiment.document.label);
+                  console.log(sentresp);
+                  const emotoutput = JSON.stringify(nluresponse.emotion.document.emotion);
+                  console.log(emotoutput);
+                  result.json({
+                    data: cleanString,
+                    ocropt: ocrtext,
+                    sentiment: sentresp,
+                    emotion: emotoutput
+                  });
+                }
+              });
+            }
+          });
         }
-    });
+      });
+    }
+  });
 });
 const port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 application.listen(port, function () {
-    console.log("Server running on port: %d", port);
+  console.log("Server running on port: %d", port);
 });
